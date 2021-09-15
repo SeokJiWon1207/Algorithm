4b4c7b39420dce8c6c3bb7bd617573513a7311f8
SeokJiWon1207 <syt1114@naver.com>
2021-09-15 19:54:18 +0900

    Coroutine commit

---
+++ src/Programming/Coroutine.kt
@@ -0,0 +1,41 @@
+package Programming
+
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking {
+    /*
+    // async
+    val time = measureTimeMillis {
+        val name: Deferred<String> = async { getFirstName() }
+        val lastName: Deferred<String> = async { getLastName() }
+        println("Coroutine, ${name.await()} ${lastName.await()}")
+    }
+    println("measure Time : $time")
+    */
+
+    /*
+    // runBlocking
+    val time = measureTimeMillis {
+    val name = getFirstName()
+    val lastName = getLastName()
+    println("Coroutine, $name $lastName")
+    }
+    println("measure Time : $time")
+    */
+
+}
+
+suspend fun getFirstName(): String {
+    delay(1000)
+    return "석"
+}
+
+suspend fun getLastName(): String {
+    delay(1000)
+    return "지원"
+}
+
