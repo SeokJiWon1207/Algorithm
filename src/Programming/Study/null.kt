3dc76b3b26487e50df98b59ec7bc1705c0320cd8
SeokJiWon1207 <syt1114@naver.com>
2021-12-14 22:46:38 +0900

    코틀린 문법 정리-2

--- src/Programming/Study/null.kt
+++ src/Programming/Study/null.kt
@@ -1,11 +1,21 @@
 package Programming.Study
 
 fun main(args: Array<String>) {
-    var name: String? = "Seok Ji Won"
+    /*var name: String? = "Seok Ji Won"
     name = null
     println("name is ${name?.length}")
 
     var name2: String? = "Seok Ji Won"
     name2 = null
-    println("name2 is ${name2!!.length}")
+    println("name2 is ${name2!!.length}")*/
+
+    var name3: String? = "Ji Won"
+    name3 = null
+
+    val len = if (name3 != null) name3.length else -1
+    println("name3 : $name3, len : $len")
+
+    var name4: String? = "Kotlin"
+    name4 = null
+    println("name4 : $name4, length : ${ name4?.length ?: -1 }")
 }
